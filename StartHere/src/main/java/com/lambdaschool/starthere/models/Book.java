6df4d4a08495f58cf2cfd60d3f6ecdf6ba0e1d6b
Carlos Santana <carlossantana987@outlook.com>
2019-07-26 11:38:21 -0400

    Tables set up

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/models/Book.java
@@ -0,0 +1,90 @@
+package com.lambdaschool.starthere.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Book")
+public class Book
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long bookid;
+
+    @Column (nullable = false)
+    private String bookTitle;
+    private String ISBN;
+    private int copyDate;
+
+    @ManyToMany
+    @JsonIgnoreProperties(value = "bookList")
+    @JoinTable(name = "onFile" , joinColumns = {@JoinColumn(name = "bookid")},
+    inverseJoinColumns = {@JoinColumn(name = "authorid")})
+    List<Author> authorList = new ArrayList<>();
+
+    Book()
+    {
+    }
+
+    public Book(String bookTitle, String ISBN, int copyDate, List<Author> authorList)
+    {
+        this.bookTitle = bookTitle;
+        this.ISBN = ISBN;
+        this.copyDate = copyDate;
+        this.authorList = authorList;
+    }
+
+    public long getBookid()
+    {
+        return bookid;
+    }
+
+    public void setBookid(long bookid)
+    {
+        this.bookid = bookid;
+    }
+
+    public String getBookTitle()
+    {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle)
+    {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getISBN()
+    {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN)
+    {
+        this.ISBN = ISBN;
+    }
+
+    public int getCopyDate()
+    {
+        return copyDate;
+    }
+
+    public void setCopyDate(int copyDate)
+    {
+        this.copyDate = copyDate;
+    }
+
+    public List<Author> getAuthorList()
+    {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList)
+    {
+        this.authorList = authorList;
+    }
+}
